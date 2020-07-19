eda751fb7d3b9c148357e9244c3fa918483cedca
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-19 04:04:47 -0300

    Adding App Navigation

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Image, View, ScrollView, KeyboardAvoidingView, Platform} from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
+import {useNavigation} from '@react-navigation/native'
 import logoImg from '../../assets/logo.png'
 
 import Input from '../../components/Input'
@@ -18,6 +19,9 @@ import {
 
 
 const SignIn: React.FC = () =>{
+
+    const navigation = useNavigation()
+
     return (
     <>  
         <KeyboardAvoidingView
@@ -49,7 +53,7 @@ const SignIn: React.FC = () =>{
             </ScrollView>
         </KeyboardAvoidingView>
 
-        <CreateAccountButton>
+        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
             <Icon name="log-in" size={20} color="#ff9000"/>
             <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
         </CreateAccountButton>
