e1dafe094666392cc42fca2fc11daf995f1a3969
Lucas Paulo <lucaspaulodev@gmail.com>
2020-09-03 19:55:20 -0300

    Create Appointment Screen

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
-import { View } from 'react-native'
+import { useRoute } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Feather'
+
+import { useAuth } from '../../hooks/auth'
+
+import { Container, Header, BackButton, HeaderTitle, UserAvatar } from './styles'
+
+
+interface RouteParams {
+    providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+    const {user} = useAuth()
+    const route = useRoute()
+    const {providerId} = route.params as RouteParams;
+
+
+
     return (
-        <View/>
+        <Container>
+            <Header>
+                <BackButton onPress={() => {}}>
+                    <Icon name="chevron-left" size={} color="#999591"/>
+
+                    <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+                    <UserAvatar source={{uri: user.avatar_url}}/>
+                </BackButton>
+            </Header>
+        </Container>
     )
 }
 
