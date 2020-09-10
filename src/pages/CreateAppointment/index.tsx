03471c88526ce44f4de974b290a64e79b75c239f
Lucas Paulo <lucaspaulodev@gmail.com>
2020-09-10 16:50:01 -0300

    Availability per time

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback, useState, useEffect } from 'react'
+import React, { useCallback, useState, useEffect, useMemo } from 'react'
 import { useRoute, useNavigation } from '@react-navigation/native'
 import Icon from 'react-native-vector-icons/Feather'
 import {Platform} from 'react-native'
 import DateTimePicker from '@react-native-community/datetimepicker'
+import { format } from 'date-fns'
 
 import api from '../../services/api'
 import { useAuth } from '../../hooks/auth'
@@ -92,6 +93,29 @@ const CreateAppointment: React.FC = () => {
         }
     }, [])
 
+    const morningAvailability = useMemo(() => {
+        return availability
+            .filter(({hour}) => hour < 12)
+            .map(({hour, available}) => {
+                return {
+                    hour,
+                    available,
+                    hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+                }
+            })
+    }, [availability])
+
+    const afternoonAvailability = useMemo(() => {
+        return availability
+            .filter(({hour}) => hour >= 12)
+            .map(({hour, available}) => {
+                return {
+                    hour,
+                    available,
+                    hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+                }
+            })
+    }, [availability])
     return (
         <Container>
             <Header>
@@ -137,6 +161,15 @@ const CreateAppointment: React.FC = () => {
                     />
                 )}
             </Calendar>
+
+            {morningAvailability.map(({hourFormatted }) => (
+                <Title key={hourFormatted}>{hourFormatted}</Title>
+            ))}
+
+            {afternoonAvailability.map(({hourFormatted }) => (
+                <Title key={hourFormatted}>{hourFormatted}</Title>
+            ))}
+            
         </Container>
     )
 }
