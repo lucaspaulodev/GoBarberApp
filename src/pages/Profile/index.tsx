6e4723ab4967842760d2479d247420fee073e3ca
Lucas Paulo <lucaspaulodev@gmail.com>
2020-09-13 18:42:52 -0300

    Updating avatar

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,6 +4,7 @@ import { useAuth } from '../../hooks/auth'
 import {useNavigation} from '@react-navigation/native'
 import * as Yup from 'yup'
 import Icon from 'react-native-vector-icons/Feather'
+import ImagePicker from 'react-native-image-picker'
 
 import api from '../../services/api'
 
@@ -103,11 +104,43 @@ const SignUp: React.FC = () =>{
                 'Ocorreu um erro ao atualizar o perfil, tente novamente',
             )
         }
-    }, [])
+    }, [navigation, updateUser])
+
+    const handleUpdateAvatar = useCallback(() => {
+        ImagePicker.showImagePicker({
+            title: 'Selecione um avatar',
+            cancelButtonTitle: 'Cancelar',
+            takePhotoButtonTitle: 'Usar câmera',
+            chooseFromLibraryButtonTitle: 'Escolher da galeria'
+        }, response => {
+            if (response.didCancel) {
+                return;
+            }
+
+            if (response.error) {
+                Alert.alert('Erro ao atualizar seu avatar')
+                return;
+            }
+
+            const data = new FormData();
+
+            data.append('avatar', {
+                type: 'image/jpg',
+                name: `${user.id}.jpg`,
+                uri: response.uri,
+            })
+            
+            api.patch('users/avatar', data).then(apiResponse => {
+                updateUser(apiResponse.data)
+            })
+            },
+        )
+    }, [updateUser, user.id])
 
     const handleGoBack = useCallback(() => {
         navigation.goBack()
     }, [navigation])
+    
     return (
     <>  
         <KeyboardAvoidingView
@@ -123,7 +156,7 @@ const SignUp: React.FC = () =>{
                     <BackButton onPress={handleGoBack}>
                         <Icon name="chevron-left" size={24} color="#999591"/>
                     </BackButton>
-                    <UserAvatarButton onPress={() => {}}>
+                    <UserAvatarButton onPress={handleUpdateAvatar}>
                         <UserAvatar source={{uri: user.avatar_url}}/>
                     </UserAvatarButton>
 
