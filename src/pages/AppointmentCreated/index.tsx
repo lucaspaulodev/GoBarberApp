362afbf297a702739600ae54b7bf44c4e3d1dc17
Lucas Paulo <lucaspaulodev@gmail.com>
2020-09-12 23:55:53 -0300

    AppointmentCreated Screen

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import { View } from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+
+import { Container } from './styles'
 
 const AppointmentCreated: React.FC = () => {
     return (
-        <View/>
+        <Container>
+            <Icon name="check" size={80} color="#04d361"/>
+            <Title>Agendamento concluído</Title>
+            <Description></Description>
+            <OkButton onPress={() => {}}>
+                <OkButtonText>
+
+                </OkButtonText>
+            </OkButton>
+        </Container>
     )
 }
 
